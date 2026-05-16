b8814c4fd88b7dd11389f52a5ce708f563a0ae72
Vazq-Leon <vazquez.leon2004@icloud.com>
2026-05-16 10:11:39 -0600

    Vistas para cada serializer

--- backend/vacunacion/views.py
+++ backend/vacunacion/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+# Importamos todos nuestros modelos
+from .models import CentroSalud, Paciente, RegistroVacunacion, Usuario, Vacuna
+
+# Importamos todos los traductores (serializers) que acabas de crear
+from .serializers import (
+    CentroSaludSerializer, 
+    PacienteSerializer, 
+    RegistroVacunacionSerializer, 
+    UsuarioSerializer, 
+    VacunaSerializer
+)
+
+# Creamos un viewset para cada modelo
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class PacienteViewSet(viewsets.ModelViewSet):
+    queryset = Paciente.objects.all()
+    serializer_class = PacienteSerializer
+
+class CentroSaludViewSet(viewsets.ModelViewSet):
+    queryset = CentroSalud.objects.all()
+    serializer_class = CentroSaludSerializer
+
+class VacunaViewSet(viewsets.ModelViewSet):
+    queryset = Vacuna.objects.all()
+    serializer_class = VacunaSerializer
+
+class RegistroVacunacionViewSet(viewsets.ModelViewSet):
+    queryset = RegistroVacunacion.objects.all()
+    serializer_class = RegistroVacunacionSerializer
+
