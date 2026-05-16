08feb4f89e6658d59d5953f6ae5bca9372ee6242
Vazq-Leon <vazquez.leon2004@icloud.com>
2026-05-16 15:28:12 -0600

    Correcion de serializers de acuerdo a los modelos

--- backend/vacunacion/serializers.py
+++ backend/vacunacion/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
-from .models import CentroSalud, Paciente, RegistroVacunacion, Usuario, Vacuna
+from .models import CentroSalud, Paciente, RegistroVacunacion, Usuario, Vacuna, Lote, Stock
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = Usuario
-        fields = ['id', 'nombre_usuario', 'email']
+        fields = ['id', 'username', 'email', 'rol', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = Usuario(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 class PacienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paciente
-        fields = ['id', 'nombre', 'apellido', 'fecha_nacimiento', 'CURP', 'domicilio', 'telefono']
+        fields = ['id', 'nombre', 'apellido', 'fecha_nacimiento', 'CURP', 'domicilio', 'telefono', 'sexo', 'edad']
 
 class CentroSaludSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,14 +28,21 @@ class CentroSaludSerializer(serializers.ModelSerializer):
 class VacunaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vacuna
-        fields = ['id', 'nombre', 'dosis_aplicadas', 'lote', 'dosis_requeridas']
+        fields = ['id', 'nombre', 'dosis_requeridas']
+
+class LoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lote
+        fields = ['id', 'vacuna', 'codigo', 'fecha_caducidad']
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = ['id', 'centro_salud', 'lote', 'cantidad']
 
 class RegistroVacunacionSerializer(serializers.ModelSerializer):
-    paciente = PacienteSerializer()
-    vacuna = VacunaSerializer()
-    centro_salud = CentroSaludSerializer()
+    personal = serializers.ReadOnlyField(source='personal.id')
 
     class Meta:
         model = RegistroVacunacion
-        fields = ['id', 'paciente', 'vacuna', 'centro_salud', 'fecha_vacunacion']
-
+        fields = ['id', 'paciente', 'vacuna', 'lote', 'centro_salud', 'personal', 'fecha_vacunacion']
