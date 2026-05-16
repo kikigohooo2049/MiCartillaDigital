f82aed730cbdcc46d6f9c3aa2d25f786a6f1fa14
Vazq-Leon <vazquez.leon2004@icloud.com>
2026-05-16 15:31:29 -0600

    Actualizacion de admin

--- backend/vacunacion/admin.py
+++ backend/vacunacion/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
+from .models import Usuario, Paciente, CentroSalud, Vacuna, RegistroVacunacion, Lote, Stock
 
-# Register your models here.
-
-from .models import Usuario, Paciente, CentroSalud, Vacuna, RegistroVacunacion
 admin.site.register(Usuario)
 admin.site.register(Paciente)
 admin.site.register(CentroSalud)
 admin.site.register(Vacuna)
+admin.site.register(Lote)
+admin.site.register(Stock)
 admin.site.register(RegistroVacunacion)
-
